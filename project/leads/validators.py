40c7758fe8f67be5e35a28f00a7628630f785eb5
VorobiovKir <kvorobiov89@gmail.com>
2016-01-22 15:07:25 +0200

    add static

--- project/leads/validators.py
+++ project/leads/validators.py
@@ -1,15 +1,7 @@
 import datetime
 
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
-
-
-class CardNumberValidator(RegexValidator):
-    regex = r'^[XTW0-9]{8,15}$'
-    message = _('''Card Number must contain only digitials
-        or XTW and have length be from 8 to 16 chars''')
-    code = _('invalid_card_number')
 
 
 def ExpiryDateValidator(value):
--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^leads/', include('leads.urls', namespace='leads')),
