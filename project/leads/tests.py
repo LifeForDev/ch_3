e272dfcb821ffe5b6d2264ebffcf5b953c09327e
VorobiovKir <kvorobiov89@gmail.com>
2016-01-27 15:53:10 +0200

    fix slug test

--- project/leads/tests.py
+++ project/leads/tests.py
@@ -8,14 +8,14 @@ class SlugTestCase(TestCase):
     def setUp(self):
         Lead.objects.create(name="Petr")
         Lead.objects.create(name="Petr")
-        Lead.objects.create(name="Petr")
-        Lead.objects.create(name="Petr")
         Lead.objects.create(name="create")
         Lead.objects.create(name="delete")
 
     def test_create_slug_unique(self):
-        leads_length = Lead.objects.all().count()
-        self.assertEqual(leads_length, 6)
+        leads_petr = Lead.objects.filter(name='Petr')
+        first_petr_slug = leads_petr[0].slug
+        second_petr_slug = leads_petr[1].slug
+        self.assertNotEqual(first_petr_slug, second_petr_slug)
 
     def test_create_slug_permission(self):
         result = Lead.objects.filter(slug__in=['create', 'delete'])
