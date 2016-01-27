f6ec7d6c92df8386e20a50a969b5bb0c53a5566d
VorobiovKir <kvorobiov89@gmail.com>
2016-01-27 15:25:39 +0200

    add slug test

--- project/leads/tests.py
+++ project/leads/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from .models import Lead
+
+
+class SlugTestCase(TestCase):
+    def setUp(self):
+        Lead.objects.create(name="Petr")
+        Lead.objects.create(name="Petr")
+        Lead.objects.create(name="Petr")
+        Lead.objects.create(name="Petr")
+        Lead.objects.create(name="create")
+        Lead.objects.create(name="delete")
+
+    def test_create_slug_unique(self):
+        leads_length = Lead.objects.all().count()
+        self.assertEqual(leads_length, 6)
+
+    def test_create_slug_permission(self):
+        result = Lead.objects.filter(slug__in=['create', 'delete'])
+        self.assertFalse(result)
